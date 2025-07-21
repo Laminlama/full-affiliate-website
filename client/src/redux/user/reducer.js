c02d308f50e1d87549e94bdac7e2ef85612571a0
Lamin <@yonjanlamin4@gmail.com>
2025-07-21 15:31:22 +0530

    more update

--- client/src/redux/user/reducer.js
+++ client/src/redux/user/reducer.js
@@ -1,17 +1,16 @@
 import { CLEAR_USER, SET_USER } from "./actions";
 
-export const userReducer = (state = null, action) => {
-    switch (action.type) {
-        // This case will help in supporting login case
-        case SET_USER:
-            return action.payload
+const initialState = {
+  user: null
+};
 
-        // This case will help in supporting logout usecase.
-        case CLEAR_USER:
-            return null;
-        // Handles case where other state update triggers
-        // userDetails' reducer
-        default:
-            return state;
-    }
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER:
+      return { ...state, user: action.payload };
+    case CLEAR_USER:
+      return { ...state, user: null };
+    default:
+      return state;
+  }
 };
